e43ae5458521fc63667f0bb7243435c2cfd2cbab
Subin Fernandez <subinfernandez@gmail.com>
2021-08-12 02:25:31 +0100

    Updated UI

--- src/components/about.js
+++ src/components/about.js
@@ -4,11 +4,13 @@ import styles from './about.module.scss'
 
 const About = () => {
   return (
-    <section className={styles.root}>
-      <h1>About Us</h1>
-      <p>Hello, I&apos;m the <strong>about</strong> module</p>
-      <p>I&apos;m from https://mfe-next-js-about.vercel.app/</p>
-    </section>
+    <div className={styles.container}>
+      <section className={styles.root}>
+        <h1>About Us</h1>
+        <p>Hello, I&apos;m the <strong>about</strong> module</p>
+        <p>I&apos;m from https://mfe-next-js-about.vercel.app/</p>
+      </section>
+    </div>
   )
 }
 
